85e7a9b9c35b4309f19bf14cd2700a64c2912041
MarFG1 <202021551@tese.edu.mx>
2024-04-23 12:16:01 -0600

    Idiomas

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -6,7 +6,54 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  languages = [
+    { code: 'es', name: 'Español' },
+    { code: 'en', name: 'Inglés' },
+    { code: 'fr', name: 'Francés' },
+    { code: 'de', name: 'Alemán' },
+    { code: 'it', name: 'Italiano' }
+  ];
+  niveles: number[] = [1, 2, 3, 4, 5];
+  datosAgregados: any[] = [];
+
+  selectedLanguage!: string;
+  nombre: string = '';
+  nivel: number=1;
+  nombreEmpresa: string = '';
+  fechaInicio!: string;
+  fechaTermino!: string;
 
   constructor() {}
+
+  languageChanged() {
+    console.log('Se seleccionó el idioma:', this.selectedLanguage);
+  }
+  submitForm() {
+    const nuevoDato = {
+      selectedLanguage: this.selectedLanguage,
+      nombre: this.nombre,
+      nivel: this.nivel,
+      nombreEmpresa: this.nombreEmpresa,
+      fechaInicio: this.fechaInicio,
+      fechaTermino: this.fechaTermino
+    };
+    this.datosAgregados.push(nuevoDato);
+    this.selectedLanguage = '';
+    this.nombre = '';
+    this.nivel = 1;
+    this.nombreEmpresa = '';
+    this.fechaInicio = '';
+    this.fechaTermino = '';
+    console.log('Datos agregados:', this.datosAgregados);
+  }
+  onFechaInicioChange(event: CustomEvent) {
+    this.fechaInicio = event.detail.value; 
+    console.log('Fecha de inicio:', this.fechaInicio);
+  }
+
+  onFechaTerminoChange(event: CustomEvent) {
+    this.fechaTermino = event.detail.value; 
+    console.log('Fecha de término:', this.fechaTermino);
+  }
 
 }
